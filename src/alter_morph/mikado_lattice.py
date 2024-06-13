5ca764ca2890cfe652960b1e9e15f1895606f250
dpreuo <49160657+dpreuo@users.noreply.github.com>
2024-06-13 15:02:06 +0200

    added relaxation and koala wrapper

--- src/alter_morph/mikado_lattice.py
+++ src/alter_morph/mikado_lattice.py
@@ -1,6 +1,49 @@
-
 import numpy as np
+import koala as kl
+from koala.lattice import Lattice
 
+
+# TODO -- Fix the shape part her eto make it more square
+def koala_mikado(density, relax_steps=0):
+    """Wrap the mikado generation function and return a koala lattice. Normalize to the [0, 1] interval.
+
+    Parameters
+    ----------
+    R : float
+        Radius of the circular area of the graph
+    density : float
+        Expected density of the nodes. The actual number of nodes is random with
+        Poisson distribution.
+    shape : callable (optional)
+        Boolean function of 2D coordinates that specifies the shape of the graph
+        area. Should be inside the radius `R` circle. By default the radius `R`
+        circle is returned. Should take array inputs.
+    relax_steps : integer, default 0
+        Relaxation steps to perform on the graph. Nodes on the edges (with less
+        than 4 neighbors) are left fixed, points inside are moved to the barycenter
+        of their neighbors in each step.
+
+    Returns
+    -------
+    klattice : koala.Lattice
+        A koala lattice object.
+    """
+    coords, bonds = random_line_graph(0.5, density, None, relax_steps)
+
+    # move coords to [0, 1] interval
+    x_max = np.max(coords[:, 0])
+    y_max = np.max(coords[:, 1])
+    xmin = np.min(coords[:, 0])
+    ymin = np.min(coords[:, 1])
+
+    coords[:, 0] = (coords[:, 0] - xmin) / (x_max - xmin)
+    coords[:, 1] = (coords[:, 1] - ymin) / (y_max - ymin)
+
+    coords = (coords - 0.5) * 0.98 + 0.5
+
+    klattice = Lattice(coords, bonds, np.zeros_like(bonds))
+    return klattice
+    
 
 def get_drs(coords, coords2=None, period=None):
     if coords2 is None:
@@ -13,6 +56,9 @@ def get_drs(coords, coords2=None, period=None):
     if period is not None:
         drs = drs - period[:, None] * np.sign(drs) * (np.abs(drs) > period[:, None]/2)
     return drs
+
+
+
 
 def relaxation_2(coords, bonds, force = None, force_params = None, z_min = 6):
     dim = np.size(coords, 1)
@@ -58,6 +104,56 @@ def cut_graph(coords, bonds, shape):
     new_bonds = index_map[new_bonds]
     return new_coords, new_bonds
 
+def relaxation(coords, bonds, prop=1, repeats=5):
+    """Relax the coordinates of the nodes in the graph.
+
+    Parameters
+    ----------
+    coords : np.array
+        Array of coordinates of the nodes.
+    bonds : np.array
+        Array of bonds between the nodes.
+    prop : float, default 1
+        Proportion of the movement to the barycenter of the neighbors.
+    repeats : int, default 5
+        Number of relaxation steps to perform.
+
+    Returns
+    -------
+    coords : np.array
+        Array of relaxed coordinates.
+    bonds : np.array
+        Array of bonds between the nodes.
+    """ 
+
+    neighbours_list = np.zeros([len(coords), 4], dtype=int)
+
+    # find the list of every site's neighbours
+    for i in range(len(coords)):
+        ind_bond, ind_pos = np.where(np.isin(bonds, i))
+        neighbour_ind = 1 - ind_pos
+        neighbours = np.pad(
+            bonds[ind_bond, neighbour_ind],
+            (0, 4 - len(ind_bond)),
+            "constant",
+            constant_values=-1,
+        )
+        neighbours_list[i] = neighbours
+
+    # move each point to the center of mass of its neighbours, n times
+    for n in range(repeats):    
+        full_coordinated = np.all(neighbours_list != -1, axis=1) 
+        positions_neighbours = coords[neighbours_list]
+        new_centers = np.mean(positions_neighbours, axis=1)
+        coords = (
+            (prop*full_coordinated[:, None]) * new_centers
+            + (1 - prop*full_coordinated[:, None]) * coords
+        )
+
+    return coords, bonds
+
+
+
 def random_line_graph(R, density, shape=None, relax_steps=0):
     """Generate fourfold coordinated graph using a random set of straight lines.
     Computation time is linear in the number of nodes. See also:
@@ -93,7 +189,7 @@ def random_line_graph(R, density, shape=None, relax_steps=0):
     # Array of intersection points of pairs of lines in polar coordinates
     rs, phis = intersection(ps, thetas, ps[:, np.newaxis], thetas[:, np.newaxis])
     # get rid of diagonal
-    rs[np.diag_indices(N)] = np.inf
+    rs[np.diag_indices(N)] = np.nan
     # connect neighbors
     # Indexing of intersections
     indx = np.zeros((N, N), dtype=int)
@@ -115,7 +211,7 @@ def random_line_graph(R, density, shape=None, relax_steps=0):
 
     # remove points outside radius R
     if shape is None:
-        def shape(x): np.abs(x[0]) < R and np.abs(x[1])<R
+        shape = lambda x: np.abs(x[0]) < R and np.abs(x[1])<R
         """
         def shape(x):
             if (np.abs(x[0])<R and np.abs(x[1])<R):
@@ -126,8 +222,10 @@ def random_line_graph(R, density, shape=None, relax_steps=0):
     coords, bonds = cut_graph(coords, bonds, shape)
 
     # relax the graph
-    
-    for _ in range(relax_steps):
-        coords = relaxation_2(coords, bonds, spring_2, {'l0':1/density, 'k':.3}, z_min=1)
+
+
+    coords, bonds = relaxation(coords, bonds, prop=1, repeats=relax_steps)
+    # for _ in range(relax_steps):
+    #     coords = relaxation_2(coords, bonds, spring_2, {'l0':1/density, 'k':.3}, z_min=1)
 
     return coords, bonds
